4b1afbd496f52923a3d6e8dc9875941c8dc3adb9
Krake24 <50485907+Krake24@users.noreply.github.com>
2023-01-23 14:11:19 +0000

    added tps info

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from replit import db
 
 app = Flask(__name__)
 
+discord_login_url = "https://discord.com/api/oauth2/authorize?client_id=1002659405025788035&redirect_uri=https%3A%2F%2Fdiscord-auth.krake24.repl.co%2Fcallback&response_type=code&scope=identify"
+
 if not 'logins' in db:
-  db['logins'] = {}
+    db['logins'] = {}
 
 if not 'callbacks' in db:
-  db['callbacks'] = {}
+    db['callbacks'] = {}
 
 
 @app.after_request
@@ -20,72 +22,98 @@ def apply_caching(response):
     response.headers.add("Access-Control-Allow-Methods", "*")
     return response
 
+
 @app.route('/')
 def main():
-  return 'alive'
+    return 'alive'
+
 
 @app.route('/invalidateall')
 def invalidateAll():
-  db['logins'] = {}
-  return 'logins invalidated'
+    db['logins'] = {}
+    return 'logins invalidated'
+
 
 @app.route('/invalidate')
 def invalidate():
-  state = request.args['state']
-  db['logins'][state] = {}
-  return json.dumps('login for ' + state + ' invalidated', ensure_ascii=False).encode('utf8')
+    state = request.args['state']
+    db['logins'][state] = {}
+    return json.dumps('login for ' + state + ' invalidated',
+                      ensure_ascii=False).encode('utf8')
 
 
 @app.route('/me')
 def getUser():
-  state = request.args['state']
-  if not state or state not in db['logins']:
-    return json.dumps({}, ensure_ascii=False).encode('utf8')
-  print("get user for state: " + state)
-  return json.dumps(dict(db['logins'][state]), ensure_ascii=False).encode('utf8')
+    state = request.args['state']
+    if not state or state not in db['logins']:
+        return json.dumps({}, ensure_ascii=False).encode('utf8')
+    print("get user for state: " + state)
+    return json.dumps(dict(db['logins'][state]),
+                      ensure_ascii=False).encode('utf8')
+
 
 @app.route('/login')
 def login():
-  state = request.args['state']
-  print("logging in for state: " + state)
-  db['callbacks'][state] = request.args['callback']
-  return redirect("https://discord.com/api/oauth2/authorize?client_id=1002659405025788035&redirect_uri=https%3A%2F%2Fdiscord-auth.krake24.repl.co%2Fcallback&response_type=code&scope=identify&state=" + state, code=302)
+    state = request.args['state']
+    print("logging in for state: " + state)
+    db['callbacks'][state] = request.args['callback']
+    guild = ""
+    if 'guilds' in request.args:
+        guild = "%20guilds"
+    return redirect(discord_login_url + guild + "&state=" + state, code=302)
+
 
 def exchange_code(code, state):
-  data = {
-    'client_id': '1002659405025788035',
-    'client_secret': os.environ['discord.secret'],
-    'grant_type': 'authorization_code',
-    'code': code,
-    'state' : state,
-    'redirect_uri': 'https://discord-auth.krake24.repl.co/callback'
-  }
-  headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-  r = requests.post('https://discord.com/api/v10/oauth2/token', data=data, headers=headers)
-  r.raise_for_status()
-  result = r.json()
+    data = {
+        'client_id': '1002659405025788035',
+        'client_secret': os.environ['discord.secret'],
+        'grant_type': 'authorization_code',
+        'code': code,
+        'state': state,
+        'redirect_uri': 'https://discord-auth.krake24.repl.co/callback'
+    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    r = requests.post('https://discord.com/api/v10/oauth2/token',
+                      data=data,
+                      headers=headers)
+    r.raise_for_status()
+    result = r.json()
 
-  headers= {
-    'Authorization' : result['token_type'] + ' ' + result['access_token']
-  }
-  r = requests.get("https://discord.com/api/v10/oauth2/@me", headers = headers)
-  result = r.json()
-  print(result)
-  db['logins'][state] = result['user']
-  return redirect(db['callbacks'][state], 302)
+    headers = {
+        'Authorization': result['token_type'] + ' ' + result['access_token']
+    }
+    r = requests.get("https://discord.com/api/v10/oauth2/@me", headers=headers)
+    result = r.json()
+    print(result)
+    db['logins'][state] = result['user']
+
+    try:
+        id = db['logins'][state]['id']
+        print("calling: " +
+              "https://ot3d-bot-prod-d3bot-s29oax.mo4.mogenius.io/user/" + id +
+              "/tps")
+        r = requests.get(
+            "https://ot3d-bot-prod-d3bot-s29oax.mo4.mogenius.io/user/" + id +
+            "/tps")
+        content = r.json()
+        db['logins'][state]['tps'] = content
+    except:
+        print("could not find tps state")
+
+    return redirect(db['callbacks'][state], 302)
+
 
 @app.route('/callback')
 def callback():
-  try:
-    print("called back")
-    args = request.args
-    state = args['state']
-    code = args['code']
-    return exchange_code(code, state)
-  except Exception as e:
-    print(e)
-    return redirect(db['callbacks'][state], 302)
+    try:
+        print("called back")
+        args = request.args
+        state = args['state']
+        code = args['code']
+        return exchange_code(code, state)
+    except Exception as e:
+        print(e)
+        return redirect(db['callbacks'][state], 302)
+
 
 app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
